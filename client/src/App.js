a122e2a433ac98e0bd2dd54f7c5b4885cb861a21
Abdul Basit Mehtab <abasitm.1993@gmail.com>
2022-01-15 20:51:49 +0500

    Initial commit

---
+++ client/src/App.js
@@ -0,0 +1,82 @@
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+import "./App.css";
+
+const localURL = "http://localhost:3001";
+
+function App() {
+  const [foodName, setFoodName] = useState("");
+  const [days, setDays] = useState(0);
+  const [newFoodName, setNewFoodName] = useState("");
+  const [foodList, setFoodList] = useState([]);
+
+  useEffect(() => {
+    axios.get(`${localURL}/read`).then((response) => {
+      // console.log(response);
+      setFoodList(response.data);
+    });
+  }, []);
+
+  const addToList = () => {
+    // console.log(foodName + days);
+    axios.post(`${localURL}/insert`, {
+      foodName: foodName,
+      days: days,
+    });
+  };
+
+  const updateFood = (id) => {
+    axios.put(`${localURL}/update`, {
+      id: id,
+      newFoodName: newFoodName,
+    });
+  };
+
+  const deleteFood = (id) => {
+    axios.delete(`${localURL}/delete/${id}`);
+  };
+
+  return (
+    <div className="App">
+      <h1>CRUD App with MERN</h1>
+
+      <label>Food Name:</label>
+      <input
+        type="text"
+        onChange={(event) => {
+          setFoodName(event.target.value);
+        }}
+      />
+      <label>Days Since You Ate It:</label>
+      <input
+        type="number"
+        onChange={(event) => {
+          setDays(event.target.value);
+        }}
+      />
+      <button onClick={addToList}>Add To List</button>
+
+      <h1>Food List</h1>
+
+      {foodList.map((val, key) => {
+        return (
+          <div key={key} className="food">
+            <h1>{val.foodName}</h1>
+            <h1>{val.daysSinceIAte}</h1>
+            <input
+              type="text"
+              placeholder="New Food Name..."
+              onChange={(event) => {
+                setNewFoodName(event.target.value);
+              }}
+            />
+            <button onClick={() => updateFood(val._id)}>Update</button>
+            <button onClick={() => deleteFood(val._id)}>Delete</button>
+          </div>
+        );
+      })}
+    </div>
+  );
+}
+
+export default App;
